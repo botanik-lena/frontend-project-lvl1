91e445d793e3c1f927342e310d91fc35432bb69b
Elena <botanik-lena@yandex.ru>
2021-11-13 01:35:36 +0300

    Refactoring brain-games.js and cli.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,10 +1,4 @@
 #!/usr/bin/env node
 import hello from '../src/cli.js';
 
-const welcome = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = hello();
-  return userName;
-};
-
-export default welcome;
+hello();
